d8933763da41e362985d2431b856cd39547488ff
rzariwal <50871325+rzariwal@users.noreply.github.com>
2024-08-21 11:42:01 +0800

    Update MetricData.jsx

--- MetricData.jsx
+++ MetricData.jsx
@@ -3,7 +3,6 @@ const renderMetricsTable = (metricsData) => {
         return metric[key] !== undefined ? metric[key] : '';
     };
 
-    // Extract all possible metric types to ensure they are displayed correctly
     const extractMetricRows = (metrics) => {
         return metrics.flatMap(metric => {
             const metricRows = [];
@@ -24,11 +23,68 @@ const renderMetricsTable = (metricsData) => {
     const doraMetrics = extractMetricRows(metricsData.filter(metric => metric.Dimension === "DORA"));
     const agileMetrics = extractMetricRows(metricsData.filter(metric => metric.Dimension === "AGILE"));
 
+    const mergeCells = (metrics) => {
+        let mergedRows = [];
+        let previousDimension = null;
+        let previousMetricType = null;
+        let dimensionRowSpan = 0;
+        let metricTypeRowSpan = 0;
+
+        metrics.forEach((metric, index) => {
+            const isNewDimension = metric.Dimension !== previousDimension;
+            const isNewMetricType = metric.MetricType !== previousMetricType;
+
+            if (isNewDimension) {
+                if (dimensionRowSpan > 0) {
+                    mergedRows[mergedRows.length - dimensionRowSpan].rowSpan = dimensionRowSpan;
+                }
+                previousDimension = metric.Dimension;
+                dimensionRowSpan = 1;
+            } else {
+                dimensionRowSpan++;
+            }
+
+            if (isNewMetricType) {
+                if (metricTypeRowSpan > 0) {
+                    mergedRows[mergedRows.length - metricTypeRowSpan].metricTypeRowSpan = metricTypeRowSpan;
+                }
+                previousMetricType = metric.MetricType;
+                metricTypeRowSpan = 1;
+            } else {
+                metricTypeRowSpan++;
+            }
+
+            mergedRows.push({
+                ...metric,
+                rowSpan: 1,
+                metricTypeRowSpan: 1,
+                isFirstDimension: isNewDimension,
+                isFirstMetricType: isNewMetricType,
+            });
+        });
+
+        if (dimensionRowSpan > 0) {
+            mergedRows[mergedRows.length - dimensionRowSpan].rowSpan = dimensionRowSpan;
+        }
+
+        if (metricTypeRowSpan > 0) {
+            mergedRows[mergedRows.length - metricTypeRowSpan].metricTypeRowSpan = metricTypeRowSpan;
+        }
+
+        return mergedRows;
+    };
+
     const renderRows = (metrics) => {
-        return metrics.map((metric, index) => (
+        const mergedMetrics = mergeCells(metrics);
+
+        return mergedMetrics.map((metric, index) => (
             <tr key={index}>
-                <td>{metric.Dimension}</td>
-                <td>{metric.MetricType}</td>
+                {metric.isFirstDimension && (
+                    <td rowSpan={metric.rowSpan}>{metric.Dimension}</td>
+                )}
+                {metric.isFirstMetricType && (
+                    <td rowSpan={metric.metricTypeRowSpan}>{metric.MetricType}</td>
+                )}
                 <td>{metric.Metric}</td>
                 <td>{metric.Value}</td>
             </tr>
@@ -36,20 +92,23 @@ const renderMetricsTable = (metricsData) => {
     };
 
     return (
-        <table className="metrics-table">
-            <thead>
-                <tr>
-                    <th>Dimension</th>
-                    <th>MetricType</th>
-                    <th>Metric</th>
-                    <th>Value</th>
-                </tr>
-            </thead>
-            <tbody>
-                {renderRows(doraMetrics)}
-                {renderRows(agileMetrics)}
-            </tbody>
-        </table>
+        <div className="application-detail">
+            <h2 className="metrics-heading">ApplicationMetrics</h2>
+            <table className="metrics-table">
+                <thead>
+                    <tr>
+                        <th>Dimension</th>
+                        <th>MetricType</th>
+                        <th>Metric</th>
+                        <th>Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderRows(doraMetrics)}
+                    {renderRows(agileMetrics)}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
