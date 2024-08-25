c2dc53b25688c2db4b98bb52b11a3c730917dd9d
rzariwal <50871325+rzariwal@users.noreply.github.com>
2024-08-25 11:41:55 +0800

    Update ApiController.java

--- ApiController.java
+++ ApiController.java
@@ -8,10 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.net.ssl.SSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
