dde3aff7e8f8f85fa412920c9cc9c70bc2ad36b9
rzariwal <50871325+rzariwal@users.noreply.github.com>
2024-08-25 12:16:26 +0800

    Update AppConfig.java

--- AppConfig.java
+++ AppConfig.java
@@ -1,14 +1,56 @@
-package com.example.yourapplication.config;
-
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.SSLContext;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
+
 @Configuration
-public class AppConfig {
+public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplateWithDisabledSSL() throws Exception {
+        // Create a TrustManager that trusts all certificates
+        TrustManager[] trustAll = new TrustManager[]{
+            new X509TrustManager() {
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {
+                }
+            }
+        };
+
+        // Build SSL context with the TrustManager
+        SSLContext sslContext = SSLContextBuilder.create()
+                .loadTrustMaterial((chain, authType) -> true)
+                .build();
+
+        // Create HttpClient with the custom SSL context
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLContext(sslContext)
+                .build();
+
+        // Create HttpComponentsClientHttpRequestFactory using the HttpClient
+        HttpComponentsClientHttpRequestFactory requestFactory = 
+                new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return new RestTemplate(requestFactory);
     }
 }
+
